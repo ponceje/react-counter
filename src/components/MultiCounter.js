578a66cbbb29e15ea67cca00b1cacbfd968992c5
PONCEJE <ponceje@oocl.com>
2020-10-26 21:51:24 +0800

    [react-counter] [Jeyem] Feature: Added MultiCounter

---
+++ src/components/MultiCounter.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import CounterGroup from './CounterGroup';
+import CounterGroupSum from './CounterGroupSum';
+import CounterSizeGenerator from './CounterSizeGenerator';
+class MultiCounter extends Component {
+constructor(props){
+    super(props);
+    this.state={size:0,sum:0
+    };
+}
+
+onGenerate=(size)=>{
+    console.log(size);
+    this.setState({size})
+}
+
+toAdd=(value)=>{
+this.setState((prevState)=> ({sum:prevState.sum+value}));
+}
+
+    render() {
+        return (
+            <div>
+                <CounterSizeGenerator onGenerate={this.onGenerate}/>
+                <CounterGroupSum sum={this.state.sum}/>
+                <CounterGroup size={this.state.size} toAdd={this.toAdd}/>
+            </div>
+        );
+    }
+}
+
+export default MultiCounter;
